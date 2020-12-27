82f78752e5c8d96017af0e5cf155a8c78fa6b1e9
KyoujinKun <74514898+KyoujinKun@users.noreply.github.com>
2020-12-27 17:05:55 +0700

    Add files via upload

---
+++ userbot/modules/oi.py
@@ -0,0 +1,39 @@
+from time import sleep
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.oi(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`Hai Cantik..`")
+    sleep(3)
+    await typew.edit("`Boleh PM Nggak `")
+    sleep(1)
+    await typew.edit("`Sekalian No WA + IG 😘`")
+# awokwkwowwk
+    # original by @localheart edited by @GabuterTololsCH
+
+
+@register(outgoing=True, pattern='^.io(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`GW GANTENG BANGET ...`")
+    sleep(3)
+    await typew.edit("`GW GANTENG BANGET ANJIM`")
+    sleep(1)
+    await typew.edit("`GW GANTENG LEBIHIN LO`")
+# line 14
+
+
+@register(outgoing=True, pattern='^.lll(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`Di tolak buat ngeuue ...`")
+    sleep(3)
+    await typew.edit("`Tapi Gua nggak nyesel ❤️😧`")
+    sleep(1)
+    await typew.edit("`I love memki`")
+# line 14
---
+++ userbot/modules/salam.py
@@ -0,0 +1,65 @@
+from time import sleep
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@register(outgoing=True, pattern='^.PP(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit(f"**Hallo Semua Saya {DEFAULTUSER} Mengucapkan**")
+    sleep(2)
+    await typew.edit("`Assalamualaikum.....`")
+    sleep(30)
+    await msg.delete()
+# Owner @Si_Dian + @GabuterTolols
+
+
+@register(outgoing=True, pattern='^.pp(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit(f"**Hallo Semua Saya {DEFAULTUSER} Mengucapkan**")
+    sleep(2)
+    await typew.edit("`Assalamualaikum.....`")
+    sleep(30)
+    await msg.delete()
+# Owner @Si_Dian + @GabuterTolols
+
+
+@register(outgoing=True, pattern='^.LL(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Astaghfirulloh Jawab Salam Dong...`")
+    sleep(1)
+    await typew.edit("`Waallaikumsalam......`")
+    sleep(30)
+    await msg.delete()
+# Owner @Si_Dian + @GabuterTololsCH
+
+
+@register(outgoing=True, pattern='^.ll(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Astaghfirulloh Jawab Salam Dong...`")
+    sleep(1)
+    await typew.edit("`Waallaikumsalam.....`")
+    sleep(30)
+    await msg.delete()
+# Owner @Si_Dian + @GabuterTololsCH
+
+
+CMD_HELP.update({
+    "salam":
+    "`.pp/PP`\
+\nUsage: Untuk Memberi salam.\
+\n\n`.LL/.ll`\
+\nUsage: Untuk Menjawab Salam."
+})
